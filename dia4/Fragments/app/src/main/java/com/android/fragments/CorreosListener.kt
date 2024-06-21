f7e8a2e460f8e232606c293d5b52391ebe466994
vidapogosoft <vidapogosoft@hotmail.com>
2024-06-21 18:48:33 -0500

    fragments v3

---
+++ dia4/Fragments/app/src/main/java/com/android/fragments/CorreosListener.kt
@@ -0,0 +1,8 @@
+package com.android.fragments
+
+import com.android.fragments.Correo
+
+interface CorreosListener {
+
+    fun onCorreoSeleccionado(correo: Correo)
+}
--- dia4/Fragments/app/src/main/java/com/android/fragments/FragmentListado.kt
+++ dia4/Fragments/app/src/main/java/com/android/fragments/FragmentListado.kt
@@ -1,7 +1,53 @@
 package com.android.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 class FragmentListado : Fragment() {
+
+    private lateinit var listado : RecyclerView
+    var listener : CorreosListener? = null
+
+    private val datos = MutableList(10)
+    {i -> Correo("Persona $i","Asunto del correo $i"
+        ,"texto del correo $i") }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return inflater.inflate(R.layout.fragment_listado, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        listado = requireView().findViewById(R.id.LstListado)
+
+        val adaptador = AdaptadorCorreo(datos)
+        { listener?.onCorreoSeleccionado(it) }
+
+        listado.adapter = adaptador
+    }
+
+    fun setCorreosListener(listen: CorreosListener)
+    {
+        listener = listen
+    }
+
+    fun setCorreosListener(seleccion : (Correo) -> Unit)
+    {
+        listener = object:CorreosListener{
+            override fun onCorreoSeleccionado(correo: Correo) {
+                seleccion(correo)
+            }
+        }
+    }
 
 }
