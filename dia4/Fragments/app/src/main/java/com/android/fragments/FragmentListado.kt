d9712238beee8c1909b8648eb99852a3e40c5006
vidapogosoft <vidapogosoft@hotmail.com>
2024-06-21 19:25:08 -0500

    fragment final

--- dia4/Fragments/app/src/main/java/com/android/fragments/FragmentListado.kt
+++ dia4/Fragments/app/src/main/java/com/android/fragments/FragmentListado.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class FragmentListado : Fragment() {
@@ -32,6 +33,8 @@ class FragmentListado : Fragment() {
 
         val adaptador = AdaptadorCorreo(datos)
         { listener?.onCorreoSeleccionado(it) }
+
+        listado.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
 
         listado.adapter = adaptador
     }
