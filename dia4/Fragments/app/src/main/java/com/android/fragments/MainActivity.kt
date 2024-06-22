3e2f3c8a6cf183d3db0ba10f9afb094760707ac2
vidapogosoft <vidapogosoft@hotmail.com>
2024-06-21 19:02:54 -0500

    fragment v4

--- dia4/Fragments/app/src/main/java/com/android/fragments/MainActivity.kt
+++ dia4/Fragments/app/src/main/java/com/android/fragments/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.android.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +14,24 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val frgListado = supportFragmentManager.findFragmentById(R.id.frgListado)
+        val frgListado = supportFragmentManager.findFragmentById(R.id.frgListado) as FragmentListado
 
+        frgListado.setCorreosListener {
 
+            val frgDetalle: Fragment? = supportFragmentManager.findFragmentById(R.id.FrgDetalle)
 
+            if (frgDetalle != null)
+            {
+                (frgDetalle as FragmentDetalle).mostrarDetalle(it.texto)
+            }
+            else{
+
+                val i = Intent(this, DetalleActivity::class.java)
+
+                i.putExtra(DetalleActivity.Extra_texto, it.texto)
+                startActivity(i)
+
+            }
+        }
     }
 }
